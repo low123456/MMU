d57e5acfd624070c7f675538086090281882a010
low123456 <lowtq2004@gmail.com>
2025-11-13 09:35:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit as st st.title("🎈 My new app")
+import streamlit as st
+   st.title("🎈 My new app")
 
-if st.button("Show Message"): 
-  st.info("This is your message box!")
+   if st.button("Show Message"):
+     st.info("This is your message box!")
